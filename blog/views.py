f80b8825e40d0f7cf0648627eeedc718223bc5d7
AizatKarimzhanova <aizatkarimova00@gmail.com>
2026-03-06 21:25:25 +0600

    first commit

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def hello_world_view(request):
+    if request.method == 'GET':
+        return HttpResponse('Hello World')
+    
+
